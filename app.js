05f8606efb32b3ee00e34631b919bcdc91e2f596
Ridma Pawan <ridma.mc@gmail.com>
2025-11-24 22:51:19 +0530

    created the front end of the app

---
+++ app.js
@@ -0,0 +1,13 @@
+// grab a hold of all rate scores and submit button
+// convert the string inside each button to its number
+// when clicked make the selected buttom white on default
+// render out the value that was selected when submitting
+
+
+let rating = document.querySelector(".score-btn")
+
+
+
+rating.addEventListener("click", function(e){
+    console.log(e.target)
+})
