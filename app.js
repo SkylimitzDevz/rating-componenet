270cd79b323f3e4a8916e10257efd78ae49bc2af
Ridma Pawan <ridma.mc@gmail.com>
2025-11-25 10:50:46 +0530

    Made the app fully functional & responsive

--- app.js
+++ app.js
@@ -4,10 +4,39 @@
 // render out the value that was selected when submitting
 
 
-let rating = document.querySelector(".score-btn")
+var rating = document.querySelectorAll(".score-btn")
+const submitBtn = document.getElementById("submit-btn")
+const outputEl = document.getElementById("rating-el")
+const thankyouScreen = document.getElementById("thankyou-screen")
+const ratingScreen = document.getElementById("rating-screen")
+let btnValue = 0
 
 
+for (i = 0; i < rating.length; i++){
+    
+    rating[i].addEventListener("click", function() {
+        
+        for (j = 0; j < rating.length; j++){
+            rating[j].style.backgroundColor = ""
+            rating[j].style.color = ""
+        }
 
-rating.addEventListener("click", function(e){
-    console.log(e.target)
+        btnValue = this.dataset.value
+        this.style.backgroundColor = "var(--white-color)"
+        this.style.color = "var(--Grey-950)"
+        submitBtn.style.background = "var(--Orange-500)"
+        submitBtn.style.cursor = "pointer"
+    })
+}
+
+submitBtn.addEventListener("click", function() {
+    if(btnValue === 0){
+        submitBtn.style.cursor = "not-allowed"
+    }
+    else{
+        thankyouScreen.style.display = "flex"
+        ratingScreen.style.display = "none"
+
+        outputEl.textContent = `You selected ${btnValue} out of 5`
+    }
 })
